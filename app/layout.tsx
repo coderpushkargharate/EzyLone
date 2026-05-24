8c75698265837d8b827a73aea75d3954429c300e
coderpushkargharate <pushkargharate7@gmail.com>
2026-05-24 01:59:29 -0700

    ok

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,7 +49,17 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.className}>
       <head>
-        {/* ❌ REMOVE GOOGLE FONT LINKS */}
+   <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link rel="dns-prefetch" href="https://res.cloudinary.com" />
 
         {/* ✅ DNS PREFETCH */}
         <link rel="dns-prefetch" href="https://www.googletagmanager.com" />
